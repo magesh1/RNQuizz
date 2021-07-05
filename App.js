4a9b858264e0b450cf10d9ff4c492541dfd5bec7
magesh1 <mageshcruz@gmail.com>
2021-07-05 18:13:19 +0530

    quizzApp

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity, Button, Platform} from 'react-native';
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from "@react-navigation/stack"
